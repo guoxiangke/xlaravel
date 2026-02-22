57fd16d7f65435dfdf2d588e22d2448b2e5143f6
me@mbp.m3 <TWR-****-****-****-7F47>
2026-02-21 21:39:54 -0800

    LyAudio 系列更新

--- app/Resources/Handlers/LyAudio.php
+++ app/Resources/Handlers/LyAudio.php
@@ -7,111 +7,100 @@ use Illuminate\Support\Facades\Http;
 
 class LyAudio
 {
-    // 映射关系：关键字 => 程序代码
-    private array $keywordMap = [
-        601 => 'iba', 602 => 'fa', 603 => 'cc', 604 => 'tv', 605 => 'gg',
-        606 => 'up', 607 => 'bx', 608 => 'pp', 609 => 'ym', 610 => 'pm',
-        611 => 'sa', 612 => 'bc', 613 => 'ir', 614 => 'rt', 615 => 'fd',
-        616 => 'jr', 617 => 'pk', 618 => 'dy', 619 => 'rn', 620 => 'mw',
-        621 => 'be', 622 => 'bs', 623 => '', 624 => 'dr', 625 => 'th',
-        626 => 'wr', 627 => '', 628 => 'aw', 629 => 'yp', 630 => 'mg',
-        631 => 'cfbwh', 632 => 'cedna', 633 => 'cfcbp', 634 => 'cfbls',
-        35 => 'cfbsg', 636 => 'cgaal', 637 => 'hmw', 638 => '', 639 => 'yb',
-        640 => 'mpa', 641 => 'ltsnp', 642 => 'ltstpa1', 643 => 'ltstpa2',
-        644 => 'ltstpb1', 645 => 'ltstpb2', 646 => 'ds', 647 => '',
-        648 => 'wa', 649 => 'cwa', 650 => 'gt', 651 => 'ynf', 652 => 'gw',
-        653 => 'cmw', 654 => 'it', 655 => '', 656 => '', 657 => 'ka',
-        658 => '', 659 => 'pc', 660 => 'ut', 661 => '', 662 => '', 663 => '',
-        664 => 'cs', 665 => '', 666 => '', 667 => '', 668 => 'ec', 669 => '',
-        670 => '', 671 => 'vp', 672 => 'ls', 673 => '', 674 => 'pt',
-        675 => 'wc', 676 => 'ttb', 677 => '', 678 => 'sz', 679 => '',
-        680 => 'gf', 681 => 'fh', 682 => 'caabg', 683 => 'caawm',
-        684 => 'caccp', 685 => 'caatp', 686 => 'caaco', 687 => 'cacac',
-        698 => 'mn',
-    ];
 
+    //   完全空闲的编码：
+    //      - 623, 627, 631-636, 638, 647, 655, 656, 658, 661-663, 665-667, 669, 670, 673, 677, 679, 682-697, 699
     private const PROGRAMS = [
-        // 生活智慧
-        '612' => ['title' => '书香园地', 'code' => 'bc', 'category' => '生活智慧'],
-        '610' => ['title' => '天路男行客', 'code' => 'pm', 'category' => '生活智慧'],
-        '619' => ['title' => '星之导航', 'code' => 'rn', 'category' => '生活智慧'],
-        '678' => ['title' => '肋骨咏叹调', 'code' => 'sz', 'category' => '生活智慧'],
-        '604' => ['title' => '真爱世界', 'code' => 'tv', 'category' => '生活智慧'],
-        '675' => ['title' => '不孤单地球', 'code' => 'wc', 'category' => '生活智慧'],
-        '674' => ['title' => '深度泛桌派', 'code' => 'pt', 'category' => '生活智慧'],
-        '668' => ['title' => '岁月正好', 'code' => 'ec', 'category' => '生活智慧'],
-        '613' => ['title' => 'i-Radio爱广播', 'code' => 'ir', 'category' => '生活智慧'],
-        '614' => ['title' => '今夜心未眠', 'code' => 'rt', 'category' => '生活智慧'],
-        '657' => ['title' => '天使夜未眠', 'code' => 'ka', 'category' => '生活智慧'],
-        '611' => ['title' => '零点凡星', 'code' => 'sa', 'category' => '生活智慧'],
+        '生活智慧' => [
+            '612' => ['title' => '书香园地', 'code' => 'bc'],
+            '610' => ['title' => '天路男行客', 'code' => 'pm'],
+            '619' => ['title' => '星之导航', 'code' => 'rn'],
+            '678' => ['title' => '肋骨咏叹调', 'code' => 'sz'],
+            '604' => ['title' => '真爱世界', 'code' => 'tv'],
+            '675' => ['title' => '不孤单地球', 'code' => 'wc'],
+            '674' => ['title' => '深度泛桌派', 'code' => 'pt'],
+            '668' => ['title' => '岁月正好', 'code' => 'ec'],
+            '613' => ['title' => 'i-Radio爱广播', 'code' => 'ir'],
+            '614' => ['title' => '今夜心未眠', 'code' => 'rt'],
+            '657' => ['title' => '天使夜未眠', 'code' => 'ka'],
+            '611' => ['title' => '零点凡星', 'code' => 'sa'],
+        ],
 
-        // 少儿家庭
-        '605' => ['title' => '一起成长吧！', 'code' => 'gg', 'category' => '少儿家庭'],
-        '659' => ['title' => '爆米花', 'code' => 'pc', 'category' => '少儿家庭'],
-        '602' => ['title' => '欢乐下课趣', 'code' => 'fa', 'category' => '少儿家庭'],
-        '607' => ['title' => '将将！百宝书开箱', 'code' => 'bx', 'category' => '少儿家庭'],
-        '664' => ['title' => '小羊圣经故事', 'code' => 'cs', 'category' => '少儿家庭'],
-        '606' => ['title' => '亲情不断电', 'code' => 'up', 'category' => '少儿家庭'],
-        '660' => ['title' => '我们的时间', 'code' => 'ut', 'category' => '少儿家庭'],
+        '少儿家庭' => [
+            '605' => ['title' => '一起成长吧！', 'code' => 'gg'],
+            '659' => ['title' => '爆米花', 'code' => 'pc'],
+            '602' => ['title' => '欢乐下课趣', 'code' => 'fa'],
+            '607' => ['title' => '将将！百宝书开箱', 'code' => 'bx'],
+            '664' => ['title' => '小羊圣经故事', 'code' => 'cs'],
+            '606' => ['title' => '亲情不断电', 'code' => 'up'],
+            '660' => ['title' => '我们的时间', 'code' => 'ut'],
+        ],
 
-        // 诗歌音乐
-        '680' => ['title' => '午的空间', 'code' => 'gf', 'category' => '诗歌音乐'],
-        '608' => ['title' => '一起弹唱吧！', 'code' => 'pp', 'category' => '诗歌音乐'],
-        '616' => ['title' => '与你有乐', 'code' => 'jr', 'category' => '诗歌音乐'],
+        '诗歌音乐' => [
+            '680' => ['title' => '午的空间', 'code' => 'gf'],
+            '608' => ['title' => '一起弹唱吧！', 'code' => 'pp'],
+            '616' => ['title' => '与你有乐', 'code' => 'jr'],
+        ],
 
-        // 生命成长
-        '601' => ['title' => '无限飞行号', 'code' => 'iba', 'category' => '生命成长'],
-        '603' => ['title' => '空中辅导', 'code' => 'cc', 'category' => '生命成长'],
-        '620' => ['title' => '旷野吗哪', 'code' => 'mw', 'category' => '生命成长'],
-        '617' => ['title' => '牧者抱抱团', 'code' => 'pk', 'category' => '生命成长'],
-        '618' => ['title' => '献上今天', 'code' => 'dy', 'category' => '生命成长'],
-        '698' => ['title' => '馒头的对话', 'code' => 'mn', 'category' => '生命成长'],
-        '639' => ['title' => '青春良伴', 'code' => 'yb', 'category' => '生命成长'],
-        '646' => ['title' => '晨曦讲座', 'code' => 'ds', 'category' => '生命成长'],
-        '624' => ['title' => '成主学堂', 'code' => 'dr', 'category' => '生命成长'],
-        '630' => ['title' => '主啊！995！', 'code' => 'mg', 'category' => '生命成长'],
-        '640' => ['title' => '这一刻，清心', 'code' => 'mpa', 'category' => '生命成长'],
-        '628' => ['title' => '空中崇拜', 'code' => 'aw', 'category' => '生命成长'],
-        '652' => ['title' => '教会年历‧家庭崇拜', 'code' => 'gw', 'category' => '生命成长'],
-        '672' => ['title' => '燃亮的一生', 'code' => 'ls', 'category' => '生命成长'],
-        '609' => ['title' => '颜明放羊班', 'code' => 'ym', 'category' => '生命成长'],
-        '626' => ['title' => '微声盼望', 'code' => 'wr', 'category' => '生命成长'],
+        '生命成长' => [
+            '601' => ['title' => '无限飞行号', 'code' => 'iba'],
+            '603' => ['title' => '空中辅导', 'code' => 'cc'],
+            '620' => ['title' => '旷野吗哪', 'code' => 'mw'],
+            '617' => ['title' => '牧者抱抱团', 'code' => 'pk'],
+            '618' => ['title' => '献上今天', 'code' => 'dy'],
+            '698' => ['title' => '馒头的对话', 'code' => 'mn'],
+            '639' => ['title' => '青春良伴', 'code' => 'yb'],
+            '646' => ['title' => '晨曦讲座', 'code' => 'ds'],
+            '624' => ['title' => '成主学堂', 'code' => 'dr'],
+            '630' => ['title' => '主啊！995！', 'code' => 'mg'],
+            '640' => ['title' => '这一刻，清心', 'code' => 'mpa'],
+            '628' => ['title' => '空中崇拜', 'code' => 'aw'],
+            '652' => ['title' => '教会年历‧家庭崇拜', 'code' => 'gw'],
+            '672' => ['title' => '燃亮的一生', 'code' => 'ls'],
+            '609' => ['title' => '颜明放羊班', 'code' => 'ym'],
+            '626' => ['title' => '微声盼望', 'code' => 'wr'],
+        ],
 
-        // 圣经讲解
-        '621' => ['title' => '真道分解', 'code' => 'be', 'category' => '圣经讲解'],
-        '622' => ['title' => '圣言盛宴', 'code' => 'bs', 'category' => '圣经讲解'],
-        '676' => ['title' => '穿越圣经', 'code' => 'ttb', 'category' => '圣经讲解'],
-        '654' => ['title' => '与神同行', 'code' => 'it', 'category' => '圣经讲解'],
-        '681' => ['title' => '卢文心底话', 'code' => 'fh', 'category' => '圣经讲解'],
-        '629' => ['title' => '善牧良言', 'code' => 'yp', 'category' => '圣经讲解'],
-        '615' => ['title' => '泛桌茶经班', 'code' => 'fd', 'category' => '圣经讲解'],
-        '625' => ['title' => '真理之光', 'code' => 'th', 'category' => '圣经讲解'],
-        '648' => ['title' => '天路导向', 'code' => 'wa', 'category' => '圣经讲解'],
+        '圣经讲解' => [
+            '621' => ['title' => '真道分解', 'code' => 'be'],
+            '622' => ['title' => '圣言盛宴', 'code' => 'bs'],
+            '676' => ['title' => '穿越圣经', 'code' => 'ttb'],
+            '654' => ['title' => '与神同行', 'code' => 'it'],
+            '681' => ['title' => '卢文心底话', 'code' => 'fh'],
+            '629' => ['title' => '善牧良言', 'code' => 'yp'],
+            '615' => ['title' => '泛桌茶经班', 'code' => 'fd'],
+            '625' => ['title' => '真理之光', 'code' => 'th'],
+            '648' => ['title' => '天路导向', 'code' => 'wa'],
+        ],
 
-        // 课程训练
-        '641' => ['title' => '良友圣经学院（启航课程）', 'code' => 'ltsnp', 'category' => '课程训练'],
-        '642' => ['title' => '良院普及本科第一套', 'code' => 'ltstpa1', 'category' => '课程训练'],
-        '643' => ['title' => '良院普及本科第二套', 'code' => 'ltstpa2', 'category' => '课程训练'],
-        '644' => ['title' => '良院普及进深第一套', 'code' => 'ltstpb1', 'category' => '课程训练'],
-        '645' => ['title' => '良院普及进深第二套', 'code' => 'ltstpb2', 'category' => '课程训练'],
-        '671' => ['title' => '良院讲台', 'code' => 'vp', 'category' => '课程训练'],
+        '课程训练' => [
+            '641' => ['title' => '良友圣经学院（启航课程）', 'code' => 'ltsnp'],
+            '642' => ['title' => '良院普及本科第一套', 'code' => 'ltstpa1'],
+            '643' => ['title' => '良院普及本科第二套', 'code' => 'ltstpa2'],
+            '644' => ['title' => '良院普及进深第一套', 'code' => 'ltstpb1'],
+            '645' => ['title' => '良院普及进深第二套', 'code' => 'ltstpb2'],
+            '671' => ['title' => '良院讲台', 'code' => 'vp'],
+        ],
 
-        // 其他语言
-        '650' => ['title' => '恩典与真理', 'code' => 'gt', 'category' => '其他语言'],
-        '651' => ['title' => '爱在人间（云南话）', 'code' => 'ynf', 'category' => '其他语言'],
-        '649' => ['title' => '天路导向（粤、英）', 'code' => 'cwa', 'category' => '其他语言'],
-        '637' => ['title' => '旷野吗哪（客家语）', 'code' => 'hmw', 'category' => '其他语言'],
-        '653' => ['title' => '旷野吗哪（粤语）', 'code' => 'cmw', 'category' => '其他语言'],
+        '其他语言' => [
+            '650' => ['title' => '恩典与真理', 'code' => 'gt'],
+            '651' => ['title' => '爱在人间（云南话）', 'code' => 'ynf'],
+            '649' => ['title' => '天路导向（粤、英）', 'code' => 'cwa'],
+            '637' => ['title' => '旷野吗哪（客家语）', 'code' => 'hmw'],
+            '653' => ['title' => '旷野吗哪（粤语）', 'code' => 'cmw'],
+        ],
     ];
 
     public function getResourceList(): array
     {
         $list = [];
-        foreach (self::PROGRAMS as $key => $program) {
-            $list[] = [
-                'keyword' => (string) $key,
-                'title' => $program['title'],
-            ];
+        foreach (self::PROGRAMS as $category => $programs) {
+            foreach ($programs as $key => $program) {
+                $list[] = [
+                    'keyword' => (string) $key,
+                    'title' => $program['title'],
+                ];
+            }
         }
 
         return $list;
@@ -125,7 +114,8 @@ class LyAudio
         }
 
         $numKeyword = (int) substr($keyword, 0, 3);
-        $code = self::PROGRAMS[$numKeyword]['code'] ?? $this->keywordMap[$numKeyword] ?? null;
+        $program = $this->findProgram($numKeyword);
+        $code = $program['code'] ?? null;
 
         if (empty($code)) {
             return null;
@@ -134,26 +124,25 @@ class LyAudio
         return $this->getAudioProgram($numKeyword, $code);
     }
 
+    private function findProgram(int $keyword): ?array
+    {
+        foreach (self::PROGRAMS as $category => $programs) {
+            if (isset($programs[(string) $keyword])) {
+                return $programs[(string) $keyword];
+            }
+        }
+
+        return null;
+    }
+
     private function getProgramList(): ResourceResponse
     {
-        $categories = [
-            '生活智慧',
-            '少儿家庭',
-            '诗歌音乐',
-            '生命成长',
-            '圣经讲解',
-            '课程训练',
-            '其他语言',
-        ];
-
         $content = '';
 
-        foreach ($categories as $category) {
+        foreach (self::PROGRAMS as $category => $programs) {
             $content .= "====={$category}=====\n";
-            foreach (self::PROGRAMS as $key => $program) {
-                if (($program['category'] ?? '') === $category) {
-                    $content .= "【{$key}】{$program['title']}\n";
-                }
+            foreach ($programs as $key => $program) {
+                $content .= "【{$key}】{$program['title']}\n";
             }
         }
 
